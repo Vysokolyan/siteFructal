3010261a8607c0f1632e9763474674a22f16d16a
Andrii Dolhopolov <dolgopolov_a@ukr.net>
2023-04-12 14:24:39 +0300

    hide menu

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+window.onload = function () {
+    "use strict";
+    lnk1.onclick = btnDown;
+    lnk2.onclick = btnDown;
+    lnk3.onclick = btnDown;
+    lnk4.onclick = btnDown;
+    lnk5.onclick = btnDown;
+    lnk6.onclick = btnDown;
+}
+function btnDown() {
+    "use strict";
+    var myCollapse = document.getElementById('navbarNavAltMarkup');
+    var bsCollapse = new bootstrap.Collapse(myCollapse, {
+        toggle: false
+    });
+    bsCollapse.hide();
+}
