bdf3e5260446726113fc1615900330790d95ea40
Andrii Dolhopolov <dolgopolov_a@ukr.net>
2023-04-18 09:55:48 +0300

    #1 18April Delete big paddings

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,6 @@
 window.onload = function () {
     "use strict";
     lnk1.onclick = btnDown;
-    lnk2.onclick = btnDown;
     lnk3.onclick = btnDown;
     lnk5.onclick = btnDown;
     lnk6.onclick = btnDown;
